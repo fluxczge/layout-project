36a53127368f374748663b032605371e2704844e
brian <bobbybrian.cipriano@gmail.com>
2020-06-02 19:53:10 +0800

    form sample

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  firstName = 'brian';
+  lastName = 'cipriano';
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
+import { Person } from './../person';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  p1: Person[] = [];
+  // p2 = new Person(2, 'bbm', 'c');
+
+  personForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  });
+
+  constructor(
+  ) { }
 
   ngOnInit(): void {
+    this.p1.push({id: 0, firstName: 'brian', lastName: 'cipriano' } as Person);
+    this.p1.push({id: 1, firstName: 'bobby', lastName: 'cipriano2' } as Person);
   }
 
+  submitPerson() {
+    console.log(this.personForm.value);
+
+    this.p1.push({
+      id: this.p1.length,
+      firstName: this.personForm.value.firstName,
+      lastName: this.personForm.value.lastName
+    });
+  }
 }
