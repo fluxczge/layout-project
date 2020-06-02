3a05a023be48246469cbaeaa0ebd6e7c1a0a2978
brian <bobbybrian.cipriano@gmail.com>
2020-06-02 09:45:28 +0800

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
+import { FormComponent } from './form/form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'button', component: ButtonComponent},
+  { path: 'form', component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
